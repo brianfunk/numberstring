c5201b0cd98010f0ab65a726476c1932d41e28b4
Brian Funk <bfunk@nciinc.com>
2016-09-17 22:37:09 -0400

    added test coverage - different numbers

--- test/test.js
+++ test/test.js
@@ -57,6 +57,46 @@ describe('numberstring', function() {
 	it('should be zero if 0 ', function() {
 		expect( numberstring(0) ).to.equal('zero');
 	});
+	
+	it('should be five if 5 ', function() {
+		expect( numberstring(5) ).to.equal('five');
+	});
+	
+	it('should be fifteen if 15 ', function() {
+		expect( numberstring(15) ).to.equal('fifteen');
+	});
+	
+	it('should be fifty if 50 ', function() {
+		expect( numberstring(50) ).to.equal('fifty');
+	});
+	
+	it('should be fifty-five if 55 ', function() {
+		expect( numberstring(55) ).to.equal('fifty-five');
+	});
+	
+	it('should be five hundred if 500 ', function() {
+		expect( numberstring(500) ).to.equal('five hundred');
+	});
+	
+	it('should be five thousand if 5000 ', function() {
+		expect( numberstring(5000) ).to.equal('five thousand');
+	});
+	
+	it('should be fifty thousand if 50000 ', function() {
+		expect( numberstring(50000) ).to.equal('fifty thousand');
+	});
+	
+	it('should be five hundred thousand if 500000 ', function() {
+		expect( numberstring(500000) ).to.equal('five hundred thousand');
+	});
+	
+	it('should be five million if 5000000 ', function() {
+		expect( numberstring(5000000) ).to.equal('five million');
+	});
+	
+	it('should be five million five hundred fifty-five thousand five hundred fifty-five if 5555555 ', function() {
+		expect( numberstring(5555555) ).to.equal('five million five hundred fifty-five thousand five hundred fifty-five');
+	});
   
 });
 
